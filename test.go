3c255b7e9cb2ec5385f5fcfc1e5de5e5b3a16c59
vincentAffinity <yanyibin@jmgo.com>
2021-10-27 10:57:36 +0800

    test

--- test.go
+++ test.go
@@ -11,7 +11,6 @@ type student struct {
 	Age  int
 }
 
-
 func main() {
 	defer func() {
 		err := recover()
@@ -25,12 +24,12 @@ func main() {
 	s.LoadTemplate("test/templates/*")
 	s.StaticResource("/static/css", "test/static")
 
-	stu1 := &student{Name: "Geektutu", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
+	stu1 := &student{Name: "yanyibin", Age: 23}
+	stu2 := &student{Name: "ty", Age: 23}
 
 	s.Get("/student", func(c *server.Context) {
 		c.HTML(http.StatusOK, "test.tmpl", server.Content{
-			"title": "yanyibin",
+			"title":    "yanyibin",
 			"students": [2]*student{stu1, stu2},
 		})
 	})
@@ -39,36 +38,27 @@ func main() {
 		c.HTML(http.StatusOK, "css.tmpl", nil)
 	})
 
-	//group := s.SetGroup("/pre")
-	//{
-	//	group.Get("/:path/aaa", func(c *server.Context) {
-	//		fmt.Println(c.PathParams["path"])
-	//		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	//	})
-	//
-	//	group.Get("/:path/bbb/:yan", func(c *server.Context) {
-	//		c.JSON(http.StatusOK, server.Content{
-	//			"t1": c.PathParams["yan"],
-	//			"text": c.PathParams["path"],
-	//			"username": "yanyibin",
-	//			"password": "yyb",
-	//		})
-	//	})
-	//}
-	//
-	//s.Get("/:path/aaa", func(c *server.Context) {
-	//	fmt.Println(c.PathParams["path"])
-	//	c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	//})
-	//
-	//s.Get("/:path/bbb/:yan", func(c *server.Context) {
-	//	c.JSON(http.StatusOK, server.Content{
-	//		"t1": c.PathParams["yan"],
-	//		"text": c.PathParams["path"],
-	//		"username": "yanyibin",
-	//		"password": "yyb",
-	//	})
-	//})
+	group := s.SetGroup("/pre")
+	{
+
+		group.Get("/:path/bbb/:yan", func(c *server.Context) {
+			c.JSON(http.StatusOK, server.Content{
+				"t1":       c.PathParams["yan"],
+				"text":     c.PathParams["path"],
+				"username": "yanyibin",
+				"password": "yyb",
+			})
+		})
+	}
+
+	s.Get("/:path/bbb/:yan", func(c *server.Context) {
+		c.JSON(http.StatusOK, server.Content{
+			"t1":       c.PathParams["yan"],
+			"text":     c.PathParams["path"],
+			"username": "yanyibin",
+			"password": "yyb",
+		})
+	})
 
 	s.Run("localhost:9999")
 }
